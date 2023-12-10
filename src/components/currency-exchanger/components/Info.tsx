268fff7a58932754a787489dbb7710793acb7dcb
Khaled Mohamed Mowafy <45627560+KhaledMowafy@users.noreply.github.com>
2023-12-11 00:14:27 +0200

    feat(currency): add currency code to the converted amount

--- src/components/currency-exchanger/components/Info.tsx
+++ src/components/currency-exchanger/components/Info.tsx
@@ -10,7 +10,7 @@ function Info({convertedAmount, from, to, amount}: Iprops) {
     <>
       <div className="currency-exchanger__info">
         <input className="currency-exchanger__info_exchange" type="text" placeholder="1.00 EUR = XX.XX USD" value={convertedAmount>0?`1.00 ${from} = ${(convertedAmount/amount).toFixed(2)} ${to}`: ''}/>
-        <input className="currency-exchanger__info_currency" type="text" placeholder="XX.XX USD" value={convertedAmount.toFixed(2)} readOnly/>
+        <input className="currency-exchanger__info_currency" type="text" placeholder="XX.XX USD" value={`${convertedAmount.toFixed(2)} ${to}`} readOnly/>
         <button className="currency-exchanger__info_button">More Details</button>
       </div>
     </>
